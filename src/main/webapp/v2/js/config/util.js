a6eedad347b35e69bb005bb1530319ddc18a83da
Zonghan(Wu Zonghan) <zwu@coupang.com>
2017-03-10 16:13:22 +0800

    side bar prepare for nav controllers

--- src/main/webapp/v2/js/config/util.js
+++ src/main/webapp/v2/js/config/util.js
@@ -2,11 +2,7 @@
  * Created by wuzonghan on 17/2/26.
  */
 function enhance$scopeWithSideBar($scope) {
-  $scope.sideNav = false;
-  $scope.showSideNav = function () {
-    $scope.sideNav = true;
-  };
-  $scope.hideSideNav = function () {
-    $scope.sideNav = false;
-  };
+  $(document).ready(function() {
+    $(".button-collapse").sideNav()
+  });
 }
